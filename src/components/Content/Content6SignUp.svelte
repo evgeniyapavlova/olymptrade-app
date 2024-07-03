1a79d3e067beb56be1bc969153eea31ab56a0302
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-07-03 10:54:31 +0300

    Fix links

--- src/components/Content/Content6SignUp.svelte
+++ src/components/Content/Content6SignUp.svelte
@@ -1,10 +1,14 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <section>
 	<h2>Start Trading with Olymp Trade</h2>
 	<div class="text">Firstly, you'll need to register an account to unlock full functionality.</div>
 	<div class="sign-up-buttons">
-		<a href="/" class="button">Email sign-up</a>
-		<a href="/" class="button">Social media sign-up</a>
-		<a href="/" class="button">Mobile sign-up</a>
+		<a href="{base}/" class="button">Email sign-up</a>
+		<a href="{base}/" class="button">Social media sign-up</a>
+		<a href="{base}/" class="button">Mobile sign-up</a>
 	</div>
 </section>
 
